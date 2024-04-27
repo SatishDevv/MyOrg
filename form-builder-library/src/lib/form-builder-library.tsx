76a0c45365485db096d38653380e81cf3037eb92
Satishghode <satishghode9075@gmail.com>
2024-04-28 01:46:50 +0530

    create form builder library in react

---
+++ form-builder-library/src/lib/form-builder-library.tsx
@@ -0,0 +1,20 @@
+import { Route, Link } from 'react-router-dom';
+
+import styled from 'styled-components';
+
+/* eslint-disable-next-line */
+export interface FormBuilderLibraryProps {}
+
+const StyledFormBuilderLibrary = styled.div`
+  color: pink;
+`;
+
+export function FormBuilderLibrary(props: FormBuilderLibraryProps) {
+  return (
+    <StyledFormBuilderLibrary>
+      <h1>Welcome to FormBuilderLibrary!</h1>
+    </StyledFormBuilderLibrary>
+  );
+}
+
+export default FormBuilderLibrary;
