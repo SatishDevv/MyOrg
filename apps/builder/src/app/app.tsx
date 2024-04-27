2745b7e1fcba0f9eb7c663d321ceb24f301a44b3
Satishghode <satishghode9075@gmail.com>
2024-04-28 01:26:56 +0530

    ccreate an nx workspace in react

--- apps/builder/src/app/app.tsx
+++ apps/builder/src/app/app.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import NxWelcome from './nx-welcome';
 import { Route, Routes, Link } from 'react-router-dom';
-import { FormBuilderLibrary } from '@myorg/form-builder-library';
 
 const StyledApp = styled.div`
   // Your style here
@@ -10,17 +9,7 @@ const StyledApp = styled.div`
 export function App() {
   return (
     <StyledApp>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <Link to="/page-2"><NxWelcome title="builder" /></Link>
-            </div>
-          }
-        />
-        <Route path="/form-builder-library" element={<FormBuilderLibrary />} />
-      </Routes>
+      <NxWelcome title="builder" />
     </StyledApp>
   );
 }
